de5a68042fff7d7da3921ccea0cd3f8c73e9a46f
Anton Palermo <ANPBusinessmail@gmail.com>
2022-07-04 00:15:45 +0800

    solo-project: add info section

--- business-card/src/App.js
+++ business-card/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react'
 
+import Info from './components/Info'
+
 import avatar from './assets/avatar.jpg'
 
 export default function App() {
   return (
     <main>
       <div className="card">
-        <div className='img-container'>
+        <div className="img-container">
           <img className="cover" src={avatar} alt="my awesome avatar" />
-          <div className='cover-overlay'/>
+          <div className="cover-overlay" />
+        </div>
+        <div className="body-container">
+          <Info />
         </div>
       </div>
     </main>
