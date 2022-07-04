7627410b0a26bd8c7ca5aa833ca6892227acb79c
Anton Palermo <ANPBusinessmail@gmail.com>
2022-07-04 21:40:05 +0800

    destructuring props

--- contact-app/src/components/Contact.js
+++ contact-app/src/components/Contact.js
@@ -3,18 +3,18 @@ import React from 'react'
 import Phone from '../assets/phone-icon.png'
 import Mail from '../assets/mail-icon.png'
 
-export default function Contact(props) {
+export default function Contact({ img, name, contact, email }) {
   return (
     <div className="contact-card">
-      <img src={props.img} alt="contact avatar" />
-      <h3>{props.name}</h3>
+      <img src={img} alt="contact avatar" />
+      <h3>{name}</h3>
       <div className="info-group">
         <img src={Phone} alt="phone icon" />
-        <p>{props.contact}</p>
+        <p>{contact}</p>
       </div>
       <div className="info-group">
         <img src={Mail} alt="mail icon" />
-        <p>{props.email}</p>
+        <p>{email}</p>
       </div>
     </div>
   )
