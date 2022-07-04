171088fa95a5749f77b257b74e43125e9597787a
Anton Palermo <ANPBusinessmail@gmail.com>
2022-07-04 21:28:47 +0800

    contact-app: props

--- contact-app/src/components/Contact.js
+++ contact-app/src/components/Contact.js
@@ -1,36 +1,21 @@
 import React from 'react'
 
-import Fluffykins from '../assets/fluffykins.png'
-
 import Phone from '../assets/phone-icon.png'
 import Mail from '../assets/mail-icon.png'
 
-export default function Contact() {
+export default function Contact(props) {
   return (
     <div className="contact-card">
-      <img src={Fluffykins} alt="Fluffykins" />
-      <h3>Fluffykins</h3>
+      <img src={props.img} alt="contact avatar" />
+      <h3>{props.name}</h3>
       <div className="info-group">
         <img src={Phone} alt="phone icon" />
-        <p>(212) 555-2345</p>
+        <p>{props.contact}</p>
       </div>
       <div className="info-group">
         <img src={Mail} alt="mail icon" />
-        <p>fluff@me.com</p>
+        <p>{props.email}</p>
       </div>
     </div>
   )
 }
-
-// <div className="contact-card">
-//   <img src={props.img} alt="contact avatar" />
-//   <h3>{props.name}</h3>
-//   <div className="info-group">
-//     <img src={Phone} alt="phone icon" />
-//     <p>{props.contact}</p>
-//   </div>
-//   <div className="info-group">
-//     <img src={Mail} alt="mail icon" />
-//     <p>{props.email}</p>
-//   </div>
-// </div>
