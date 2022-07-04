1e7e037120b11ae4215cda6e2a873021a17d2047
Anton Palermo <ANPBusinessmail@gmail.com>
2022-07-04 14:26:48 +0800

    airbnb-clone: navbar section

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -1,5 +1,10 @@
-import './App.css'
+import React from 'react'
+import Navbar from './components/Navbar'
 
 export default function App() {
-  return <h1>App</h1>
+  return (
+    <div>
+      <Navbar />
+    </div>
+  )
 }
