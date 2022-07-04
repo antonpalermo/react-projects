934ede09f22ba6e4d0619c3cffd440eb13ee70c6
Anton Palermo <ANPBusinessmail@gmail.com>
2022-07-04 22:44:53 +0800

    airbnb-clone: props

--- airbnb-clone/src/App.js
+++ airbnb-clone/src/App.js
@@ -3,13 +3,22 @@ import React from 'react'
 import Navbar from './components/Navbar'
 // import Hero from './components/Hero'
 import Card from './components/Card'
+import Katie from './assets/katie-zaferes.png'
+
 
 export default function App() {
   return (
     <div>
       <Navbar />
       {/* <Hero/> */}
-      <Card />
+      <Card
+        img={Katie}
+        rating={5.0}
+        reviewCount={6}
+        country="USA"
+        title="Life Lessons with Katie Zaferes"
+        price={136}
+      />
     </div>
   )
 }
--- airbnb-clone/src/components/Card.js
+++ airbnb-clone/src/components/Card.js
@@ -1,23 +1,22 @@
 import React from 'react'
-import card__cover from '../assets/katie-zaferes.png'
 import star from '../assets/star.png'
 
-export default function Card() {
+export default function Card(props) {
   return (
     <div className="card">
       <div className="card__cover">
-        <img className="cover" src={card__cover} alt="card cover" />
+        <img className="cover" src={props.img} alt="card cover" />
         <span className="badge">SOLD OUT</span>
       </div>
       <div className="card__status">
         <img className="star" src={star} alt="star icon" />
-        <span>5.0</span>
-        <span className='gray'>(6) • </span>
-        <span className='gray'>USA</span>
+        <span>{props.rating}</span>
+        <span className='gray'>({props.reviewCount}) • </span>
+        <span className='gray'>{props.country}</span>
       </div>
-      <p className='card__text'>Life lessons with Katie Zaferes</p>
+      <p className='card__text'>{props.title}</p>
       <p className='card__text'>
-        <strong>From $136</strong> / person
+        <strong>From ${props.price}</strong> / person
       </p>
     </div>
   )
